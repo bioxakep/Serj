8a72cf77c6f17e8a20f991f848ec557f9b9a72ea
bioxakep <bioxakep@gmail.com>
2019-10-10 21:20:54 +0300

    Main

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ if __name__ == '__main__':
     wm = WeatherMan('Moscow')
     serj = Serj()
     serj.set_rate(200)
-<<<<<<< HEAD
     serj.say_humidity(wm.curr_hum)
     serj.say_pressure(wm.curr_pres)
     '''
     serj.say_hello('Варвара')
-=======
     serj.say_hello('Александр')
->>>>>>> f581ba988765635bc481a2343e7c622120fa2cc6
     serj.say_today_date()
     serj.say_time()
     serj.say_weather(wm.curr_temp, wm.curr_hum, wm.curr_pres, wm.wind_speed, wm.wind_dir, wm.weather_desc)
@@ -27,3 +24,4 @@ if __name__ == '__main__':
         for t in task_list:
             serj.say_task(t.text, t.datetime)
     '''
+
